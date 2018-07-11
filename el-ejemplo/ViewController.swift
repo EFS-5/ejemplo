9eb1104e0c57de8cc34a011828c2896ce3716e63
Esteban Flores <estflores1@gmail.com>
2018-07-11 14:39:07 -0600

    Equis

--- el-ejemplo/ViewController.swift
+++ el-ejemplo/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         ellabel.text = "Equis"
-        
+
     }
 
     override func didReceiveMemoryWarning() {
