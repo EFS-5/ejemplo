e553e344ac316da371695d7d66eb19bddd551b9c
Esteban Flores <estflores1@gmail.com>
2018-07-16 20:44:09 -0600

    Cambiado el texto de 'ellabel'

--- el-ejemplo/ViewController.swift
+++ el-ejemplo/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        ellabel.text = "Equis"
+        ellabel.text = "Texto de ejemplo"
     }
 
     override func didReceiveMemoryWarning() {
