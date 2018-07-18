953e80a3f757b364f28bc588c7a298065a10b10d
Esteban Flores <estflores1@gmail.com>
2018-07-18 13:27:19 -0600

    Texto cambiado a "texto de ejemplo en mac"

--- el-ejemplo/ViewController.swift
+++ el-ejemplo/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        ellabel.text = "Texto de ejemplo"
+        ellabel.text = "Texto de ejemplo en mac"
     }
 
     override func didReceiveMemoryWarning() {
